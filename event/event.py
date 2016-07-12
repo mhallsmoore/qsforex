8c77f6ba822bfd6bd23ee2ce65bb7049a535a0a9
Kennedy Mbuthia <kennedy_mbuthia@yahoo.com>
2016-07-12 22:55:53 +0300

    event: event object has the comparison operator for two different events; adding time attribute to order event; describing different priority values for the different events

--- event/event.py
+++ event/event.py
@@ -1,9 +1,16 @@
+from datetime import datetime
+
+
 class Event(object):
-    pass
+    def __lt__(self, other):
+        return self.time < other.time
 
 
 class TickEvent(Event):
     def __init__(self, instrument, time, bid, ask):
+        '''
+        This will have priority value of 30
+        '''
         self.type = 'TICK'
         self.instrument = instrument
         self.time = time
@@ -21,6 +28,10 @@ class TickEvent(Event):
 
 
 class SignalEvent(Event):
+    '''
+    This will have priority value of 20 for buy event
+    This will have priority value of 20 for sell event
+    '''
     def __init__(self, instrument, order_type, side, time):
         self.type = 'SIGNAL'
         self.instrument = instrument
@@ -39,12 +50,16 @@ class SignalEvent(Event):
 
 
 class OrderEvent(Event):
+    '''
+    This will have priority value of 10
+    '''
     def __init__(self, instrument, units, order_type, side):
         self.type = 'ORDER'
         self.instrument = instrument
         self.units = units
         self.order_type = order_type
         self.side = side
+        self.time = datetime.now()
 
     def __str__(self):
         return "Type: %s, Instrument: %s, Units: %s, Order Type: %s, Side: %s" % (
