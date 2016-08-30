5cefebcc6518918c94ba9d3b3f30e74a6f801cb7
terzim <ma.terzi@tiscali.it>
2016-08-30 07:00:31 +0100

    Update gui.py

--- gui/gui.py
+++ gui/gui.py
@@ -1,1 +1,21 @@
+try:
+  from Tkinter import *
+except ImportError:
+  from tkinter import *
+  
+TradeSettings = {
+equity
+
+risk
+
+avl_pairs = [ ] 
+
+avl_strategies
+}
+
+ConfigSettings = 
+{
+  
+  
+  }
 
